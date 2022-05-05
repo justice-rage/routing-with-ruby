7a516909f5a1f972c8928ca7a53a67a396b1a7c2
justicepelteir <justice.douglas@hey.com>
2022-05-05 13:37:43 -0700

    add route schematics

--- app.rb
+++ app.rb
@@ -14,14 +14,21 @@ get('/') do
             2) Submit button - add word to be displayed 
                with editable definition or definitions"
   end
+
+  post('/words') do
+    "Word List - MVP:
+    1) This route will add a word to our list of words on our home page.
+        a. Unaccesible by typing in the url
+        b. Will use /new-word-form specifying a POST action to reach this route."
+  end
   
   get('/words/:id') do
     "Word Page - MVP:
-        1) This route will show a specific word based on its ID. The value of 
-           ID here is #{params[:id]}
+        1) This route will take a user to a word specific page based on its ID. 
+        The value of ID here is #{params[:id]}
         2) Create a word definition form with an ID of #{params[:id]}:
-            a. add a definition
-            b. edit a deifintion
-            c. delete a definition
-            d. delete word option"
+            a. add a definition - get(/words/:id)
+                · Unsure of route path for adding sole definitions
+            b. update/edit a deifintion - patch(/words/:id)
+            c. delete a definition - delete(/words/:id)
   end
