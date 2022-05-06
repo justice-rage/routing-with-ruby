99affea19879b30a8a30d42f4e73f9843860d9c9
justicepelteir <justice.douglas@hey.com>
2022-05-05 17:31:29 -0700

    create clear data test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -11,7 +11,24 @@ describe('#save') do
     end
 end
 
+describe('.clear') do
+    it("clears all words") do 
+        word1 = Word.new("bird", "organism with feathers", nil)
+        word1.save()
+        word2 = Word.new("reptile", "organism with scales", nil)
+        word2.save()
+        Word.clear()
+        expect(Word.all).to(eq([]))
+    end
+end
+
+
 describe '#Word' do
+
+    before(:each) do
+        Word.clear()
+    end
+    
     describe('.all') do
         it("returns an empty array when there are no words") do
             expect(Word.all).to(eq([]))
