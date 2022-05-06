beb0e74d47914c33f793748637bed07bce3d62c3
justicepelteir <justice.douglas@hey.com>
2022-05-05 17:14:04 -0700

    add test passing code

--- lib/word.rb
+++ lib/word.rb
@@ -1,12 +1,24 @@
 class Word
-    @@words = {}
+    attr_reader :id, :name, :definitions
 
-    def initialize(name, definitions)
+    @@words = {}
+    @@total_rows = 0
+
+    def initialize(name, definitions, id)
         @name = name
         @definitions = definitions
+        @id = id || @@total_rows += 1
     end
 
     def self.all
         @@words.values()
+    end
+
+    def ==(word_to_compare)
+        self.name() == word_to_compare.name()
+    end
+
+    def save
+        @@words[self.id] = Word.new(self.name, self.definitions, self.id)
     end
 end
