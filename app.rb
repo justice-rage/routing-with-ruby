a0dff882e0b492d2c3842d8ae094d22c400ee14a
justicepelteir <justice.douglas@hey.com>
2022-05-07 12:28:11 -0700

    remove repeat code

--- app.rb
+++ app.rb
@@ -4,12 +4,6 @@ require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
 
-require('sinatra')
-require('sinatra/reloader')
-require('./lib/word')
-require('pry')
-also_reload('lib/**/*.rb')
-
 get('/') do
   @words = Word.all
   erb(:words)
