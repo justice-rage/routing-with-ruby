6c4a99821fc710850d52f0b72cb0dcd66acae15d
justicepelteir <justice.douglas@hey.com>
2022-05-07 10:59:32 -0700

    create word name update test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -53,4 +53,13 @@ describe '#Word' do
             expect(Word.find(word1.id)).to(eq(word1))
         end
     end
+
+    describe('#update') do
+        it("updates a word by id") do
+            word1 = Word.new("Montain", "a large geological formation", nil)
+            word1.save()
+            word1.update("mountain")
+            expect(word1.name).to(eq("A love supreme"))
+        end
+    end
 end
