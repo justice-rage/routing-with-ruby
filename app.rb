80bf3e535a5aa9591169461131fb423f74b26c5e
justicepelteir <justice.douglas@hey.com>
2022-05-07 18:23:14 -0700

    add delete and update for definitions

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
 
@@ -13,51 +14,65 @@ get('/words') do
   @words = Word.all
   erb(:words)
 end
-  
-  get('/words/new-word-form') do
-    "New Word Form - MVP: 
-        Form Structure:
-            1) User text input field - write a word
-            2) Submit button - add word to be displayed 
-               with editable definition or definitions"
 
-    erb(:new_word)
-  end
+get('/words/new') do
+  erb(:new_word)
+end
 
-  post('/words') do
-    name = params[:word_name]
-    word = Word.new(name, nil)
-    word.save()
-    @words = Word.all()
-    erb(:words)
-  end
-  
-  get('/words/:id') do
-    @word = Word.find(params[:id].to_i())
-    erb(:word)
-  end
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
 
-  get('/words/:id/edit') do
-    @word = Word.find(params[:id].to_i())
-    erb(:edit_word)
-  end
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
 
-  patch('/words/:id') do
-    @word = Word.find(params[:id].to_i())
-    @word.update(params[:name])
-    @words = Word.all
-    erb(:words)
-  end
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
 
-  delete('/words/:id') do
-    @word = Word.find(params[:id].to_i())
-    @word.delete()
-    @words = Word.all
-    erb(:words)
-  end
-  
+post('/words') do
+  name = params[:word_name]
+  word = Word.new(name, nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
 
-  get('/test') do
-    @something = "a variable"
-    erb(:whatever)
-  end
+post('/words/:id/definitions') do
+  @word= Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_name], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:name])
+  @words = Word.all
+  erb(:words)
+end
+
+patch('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.update(params[:name], @word.id)
+  erb(:word)
+end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
+
+delete('/words/:id/definitions/:definition_id') do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
