37fbb22e221ced021560640a086e96a62376dedd
justicepelteir <justice.douglas@hey.com>
2022-05-07 17:34:03 -0700

    rewrite tests for clarity

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,76 +1,88 @@
 require 'rspec'
 require 'word'
-
-describe('#save') do
-    it("saves a word") do
-        word1 = Word.new("humanoid", nil)
-        word1.save()
-        word2 = Word.new("humanoid", nil)
-        word2.save()
-        expect(Word.all).to(eq([word1, word2]))
-    end
-end
-
-describe('.clear') do
-    it("clears all words") do 
-        word1 = Word.new("bird", nil)
-        word1.save()
-        word2 = Word.new("reptile", nil)
-        word2.save()
-        Word.clear()
-        expect(Word.all).to(eq([]))
-    end
-end
-
+require 'definition'
 
 describe '#Word' do
+  before(:each) do
+    Word.clear()
+  end
 
-    before(:each) do
-        Word.clear()
+  describe('.all')do
+    it("returns an empty array when there are no words")do
+      expect(Word.all).to eq([])
     end
+  end
 
-    describe('.all') do
-        it("returns an empty array when there are no words") do
-            expect(Word.all).to(eq([]))
-        end
+  describe('#save')do
+    it("saves a word")do
+      word = Word.new("hydra", nil)
+      word.save()
+      word2 = Word.new("wyvern", nil)
+      word2.save()
+      expect(Word.all).to eq([word, word2])
     end
+  end
 
-
-    describe('#==') do
-        it("is the same word if it has the same attributes as another word ") do
-            word1 = Word.new("cat", nil)
-            word2 = Word.new("cat", nil)
-            expect(word1).to(eq(word2))
-        end
+  describe('#==')do
+    it("is the same word if it has the same attributes as another word")do
+      word = Word.new("kraken", nil)
+      word2 = Word.new("kraken", nil)
+      expect(word).to eq(word2)
     end
+  end
 
-    describe('.find') do
-        it("finds a word by id") do
-            word1 = Word.new("mammal", nil)
-            word1.save()
-            word2 = Word.new("amphibian", nil)
-            word2.save()
-            expect(Word.find(word1.id)).to(eq(word1))
-        end
+  describe('.clear') do
+    it("clears all words") do
+      word = Word.new("hydra", nil)
+      word.save()
+      word2 = Word.new("wyvern", nil)
+      word2.save()
+      Word.clear()
+      expect(Word.all).to eq([])
     end
+  end
 
-    describe('#update') do
-        it("updates a word name by id") do
-            word1 = Word.new("Montain", nil)
-            word1.save()
-            word1.update("mountain")
-            expect(word1.name).to(eq("mountain"))
-        end
+  describe('.find')do
+    it("finds a word by id")do
+      word = Word.new("hydra", nil)
+      word.save()
+      word2 = Word.new("wyvern", nil)
+      word2.save()
+      expect(Word.find(word.id)).to eq(word)
     end
+  end
 
-    describe('#delete') do
-        it("deletes a word by id") do
-            word1 = Word.new("blue", nil)
-            word1.save()
-            word2 = Word.new("red", nil)
-            word2.save()
-            word1.delete()
-            expect(Word.all).to(eq([word2]))
-        end
+  describe('#update')do
+    it("updates a word by id")do
+      word = Word.new("hydra", nil)
+      word.save()
+      word.update("jackalope")
+      expect(word.name).to eq("jackalope")
     end
+  end
+
+  describe('#delete')do
+    it("deletes a word by id")do
+      word = Word.new("hydra", nil)
+      word.save()
+      word2 = Word.new("wyvern", nil)
+      word2.save()
+      word.delete()
+      expect(Word.all).to eq ([word2])
+    end
+  end
+
+  describe('#definitions') do
+  it("returns a word's definitions") do
+    Definition.clear()
+      word = Word.new("sasquatch", nil)
+      word.save()
+      definition = Definition.new("large mystery ape", word.id, nil)
+      definition.save()
+      definition2 = Definition.new("mythical forest ape", word.id, nil)
+      definition2.save()
+      expect(word.definitions).to(eq([definition, definition2]))
+    end
+  end
+
 end
